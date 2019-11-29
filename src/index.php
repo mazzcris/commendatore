bc863a59caec505aefcf3ade8d3dc5bb9a27b933
Cristiano Mazzarini <mazzcris@users.noreply.github.com>
2019-11-29 16:16:18 +0100

    Test commit for actions

--- src/index.php
+++ src/index.php
@@ -8,5 +8,9 @@ if (!file_exists('__DIR__.\'/../output')) {
     mkdir('__DIR__.\'/../output', 0777, true);
 }
 
+dump('test');
+
+var_dump('test');
+
 $commenter = new Commenter(__DIR__.'/TestClass.php', __DIR__.'/../output/TestClass.php');
 $commenter->commentOnNewFile();
