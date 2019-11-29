110f95bab016b66f60e08fb96b35605e5ab2d6ea
Cristiano Mazzarini <mazzcris@users.noreply.github.com>
2019-11-29 16:24:47 +0100

    Update index.php

--- src/index.php
+++ src/index.php
@@ -8,9 +8,5 @@ if (!file_exists('__DIR__.\'/../output')) {
     mkdir('__DIR__.\'/../output', 0777, true);
 }
 
-dump('test_11');
-
-var_dump('test_11');
-
 $commenter = new Commenter(__DIR__.'/TestClass.php', __DIR__.'/../output/TestClass.php');
 $commenter->commentOnNewFile();
